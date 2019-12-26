169026697641e5356306a986f5d637185b431e18
Hsingyu120 <b05611012@ntu.edu.tw>
2019-12-26 18:12:08 +0800

    Create color.js

---
+++ website_final_project/js/color.js
@@ -0,0 +1,124 @@
+// create color dictionary and sbuject dictionary
+var colorarray = ['紅色', '橙色', '黃色', '綠色', '藍色', '紫色', '棕色', '黑色', '白色']
+var subjectarray = ['背景是', '文字是', '背景不是', '文字不是']
+
+var question = () => {
+    //create a complete sentance
+    var cindex = rand(0, 8)
+    var sindex = rand(0, 3)
+    var sentance = subjectarray[sindex] + colorarray[cindex]
+        //create two different colors for sentance and background
+    var BackAndSenColor = shuffleArray([0, 1, 2, 3, 4, 5, 6, 7, 8])
+    var backgroundcolor = 'background' + BackAndSenColor[0]
+    var sentancecolor = 'moji' + BackAndSenColor[1]
+        //background color is index[0]
+        //sentance color is index[1]
+
+    $('#container').empty()
+    $div = $('<div>').attr('class', backgroundcolor).addClass('anscontainer')
+        //change background every time
+    $('#container').append($div)
+
+    // change sentance every time 
+
+    $moji = $('<span>').attr('class', sentancecolor).text(sentance)
+    $div.append($moji)
+
+    // create the answer for the random statement above
+    if (sindex == 0) {
+
+        if (BackAndSenColor[0] == cindex) {
+
+            return true;
+
+        } else {
+            return false;
+        }
+
+    } else if (sindex == 1) {
+
+        if (BackAndSenColor[1] == cindex) {
+
+            return true;
+
+        } else {
+            return false;
+        }
+    } else if (sindex == 2) {
+
+        if (BackAndSenColor[0] != cindex) {
+
+            return true;
+
+        } else {
+            return false;
+        }
+    } else if (sindex == 3) {
+
+        if (BackAndSenColor[1] != cindex) {
+
+            return true;
+
+        } else {
+            return false;
+        }
+    }
+
+
+}
+
+
+
+
+
+
+$(() => {
+
+    console.log('JS ready')
+
+
+    //setInterval(change, 1000)
+
+    $('#confirm').on('click', () => {
+
+        var correct = 0;
+        var error = 0;
+        var ans = question();
+        $answer = $('<span >').attr("class", "answerspan").text('正確 : ' + correct + ' 錯誤 ： ' + error)
+        $('#answerlist').empty()
+        $('#answerlist').append($answer)
+
+
+        function keyFunction() {
+
+            if (event.keyCode == 37) {
+
+                if (false == ans) {
+                    correct++
+                } else {
+                    error++
+                }
+
+                $answer.text('正確 : ' + correct + ' 錯誤 ： ' + error)
+                ans = question()
+
+            } else if (event.keyCode == 39) {
+
+                if (true == ans) {
+                    correct++
+                } else {
+                    error++
+                }
+
+                $answer.text('正確 : ' + correct + ' 錯誤 ： ' + error)
+                ans = question();
+
+            }
+        }
+        document.onkeydown = keyFunction;
+
+
+
+    })
+
+})
