4ea5508a83ae5af59287c42f775b2c0cc226bcf7
Hsingyu120 <b05611012@ntu.edu.tw>
2019-12-30 20:50:21 +0800

    Create try.js

---
+++ final/js/try.js
@@ -0,0 +1,14 @@
+var config = {
+    apiKey: "AIzaSyAP2qFl8eWdaDijWBGJ9oFAj9MDB5NG4Lk",
+    authDomain: "quickstart-1575120625329.firebaseapp.com",
+    databaseURL: "https://quickstart-1575120625329.firebaseio.com",
+    projectId: "quickstart-1575120625329",
+    storageBucket: "quickstart-1575120625329.appspot.com",
+    messagingSenderId: "664382912294",
+    appId: "1:664382912294:web:d282b7926bfca70d1273e7",
+    measurementId: "G-80FEFGRJWQ"
+};
+
+
+firebase.initializeApp(config);
+var db = firebase.firestore();
